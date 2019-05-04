23019553b3aebd4fb3c3d6b81622835567f8e2b5
Cecilia Pasquale <cecilia.pasquale16@gmail.com>
2019-05-04 13:36:06 -0400

    1

---
+++ data/friends.js
@@ -0,0 +1,182 @@
+var friendMatch = [
+
+    {
+        name: 'Sansa',
+        photo: 'https://s1.r29static.com//bin/entry/b49/0,0,2000,2400/1090x1308,85/1804416/image.webp',
+        scores: [
+           1,
+           2,
+           3,
+           4,
+           5,
+           1,
+           2,
+           3,
+           4,
+           3,
+        ]
+    },
+
+    {
+        name: 'Arya',
+        photo: 'https://media.vanityfair.com/photos/5cc758dc1d6b8739f44c2c52/master/pass/game-of-thrones-battle-of-winterfell-tweets-arya-stark-memes.jpg',
+        scores: [
+          1,
+          1,
+          2,
+          2,
+          3,
+          3,
+          4,
+          4,
+          5,
+          5,
+
+        ]
+    },
+
+    {
+        name: 'Theon',
+        photo: 'https://upload.wikimedia.org/wikipedia/en/thumb/5/51/Theon_Greyjoy-Alfie_Allen.jpg/220px-Theon_Greyjoy-Alfie_Allen.jpg',
+        scores: [
+        1,
+        1,
+        1,
+        1,
+        1,
+        2,
+        2,
+        2,
+        2,
+        2,
+        ]
+    },
+
+    {
+        name: 'John',
+        photo: 'https://media.allure.com/photos/5ba532e30ac6c654eca1f789/16:9/w_1280,c_limit/e31a1dd95b519c8063075bf3ebfc231d685e7cd34fb0985dfabe35ab063bdeb84011b71e4ae81ada3b2eb21920dc167c.jpg',
+        scores: [
+            3,
+            3,
+            3,
+            3,
+            3,
+            4,
+            4,
+            4,
+            4,
+            4,
+        ]
+    },
+
+    {
+        name: "Dany",
+        photo: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/033884f025a2eabf0eb8c4e974b7c938bca8286540de31898566bd91bfd7af85-1-1554372064.jpg?crop=0.668xw:1.00xh;0.104xw,0&resize=480:*',
+        scores: [
+            1,
+            5,
+            5,
+            5,
+            5,
+            1,
+            1,
+            1,
+            1,
+            1,
+        ]
+    },
+
+    {
+        name: 'Cersi',
+        photo: 'https://www.telegraph.co.uk/content/dam/tv/2016/06/29/cersei2-xxlarge_trans_NvBQzQNjv4Bq8uCZQ4CwV1dl0Mg8NuwggWSDtfxxgc3CJ64yW7BKTEs.jpg',
+        scores: [
+            1,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            1,
+            1,
+            1,
+
+        ]
+    },
+
+    {
+        name: 'Sam',
+        photo: 'https://static1.squarespace.com/static/52fc05c9e4b08fc45bd99090/t/5991bb7bdb29d643eb1870af/1502722943557/',
+        scores: [
+          2,
+          2,
+          2,
+          2,
+          2,
+          2,
+          2,
+          2,
+          2,
+          2,
+
+        ]
+    },
+
+    {
+        name: 'Jamie',
+        photo: 'https://s2.r29static.com//bin/entry/b40/720x864,85/2021830/image.webp',
+        scores: [
+          3,
+          3,
+          3,
+          3,
+          3,
+          3,
+          3,
+          3,
+          3,
+          3,
+
+        ]
+    },
+
+
+    {
+        name: 'Pryanka',
+        photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiwhbeKZtxHkQE_6Fl_DKsaWomgcctwI-C5R7n5JS2UAl2cprG0w',
+        scores: [
+           4,
+           4,
+           4,
+           4,
+           4,
+           4,
+           4,
+           4,
+           4,
+           4,
+
+        ]
+    },
+
+
+    {
+        name: 'Night King',
+        photo: 'https://static.gamespot.com/uploads/original/171/1712892/3522659-got_night_king_1-e1552249379545.jpg',
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+
+        ]
+    }
+]
+
+module.exports = friendMatch;
